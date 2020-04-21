9e4d5582215f292a837fdc34b745123622913233
shiun <m10821053@yuntech.edu.tw>
2020-04-21 16:15:54 +0800

    Update

---
+++ Monte Carlo/montecarlo.py
@@ -0,0 +1,33 @@
+import random
+import math
+mean = 30
+sigma = 0.05
+time = 25
+
+d = random.uniform(0.1*sigma, 0.3*sigma)
+
+
+# for i in range(time):
+#     x = random.normalvariate(0, sigma)
+#     y_normal = mean + x
+#     print(y_normal, ' ')
+
+# for i in range(time):
+#     x = random.normalvariate(0, sigma)
+#     s = random.uniform(1.5 * sigma, 3 * sigma)
+#     y_us = mean + x + s
+#     print(y_us, ' ')
+
+# for i in range(time):
+#     x = random.normalvariate(0, sigma)
+#     d = random.uniform(0.1 * sigma, 0.3 * sigma)
+#
+#     y_ut = mean + x + d*i
+#     print(y_ut, ' ')
+
+for i in range(time):
+    x = random.normalvariate(0, sigma)
+    a = random.uniform(1.5 * sigma, 4 * sigma)
+    w = random.randint(4, 8)
+    y_cyc = mean + x + a * math.sin(2 * math.pi*i/w)
+    print(y_cyc)
